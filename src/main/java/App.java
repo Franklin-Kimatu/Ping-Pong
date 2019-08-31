4b21c8feb14118ded88f79a448bcb640a6ca6edc
Franklin-Kimatu <frankimatu@gmail.com>
2019-09-01 00:15:30 +0300

    adding a final test

---
+++ src/main/java/App.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+public class App {
+    public static void main(String[] args) {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("I am a ping pong application");
+        System.out.println("Enter a number:");
+        try{
+            String stringUserNumber= bufferedReader.readLine();
+            int intUserNumber = Integer.parseInt(stringUserNumber);
+            PingPong pingpong = new PingPong();
+            ArrayList<Object> pingpongresult = pingpong.runPingPong(intUserNumber);
+            System.out.println(pingpongresult);
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+}
