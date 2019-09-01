6f5d95d3044f0cfc915ed27f706ef7040839dc53
Franklin-Kimatu <frankimatu@gmail.com>
2019-09-01 11:26:22 +0300

    adding a comment to the user interface

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+// this carries the user interface.
 public class App {
     public static void main(String[] args) {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
